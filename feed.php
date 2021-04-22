e8ae6fc4938ca5b59714fdcc6840f4a56ee68832
Nisarg20 <70905787+Nisarg20@users.noreply.github.com>
2021-04-22 19:46:54 +0530

    Update feed.php

--- feed.php
+++ feed.php
@@ -21,6 +21,6 @@ $id=uniqid();
 $date=date("Y-m-d");
 $time=date("h:i:sa");
 $feedback = $_POST['feedback'];
-$q=mysqli_query($con,"INSERT INTO feedback VALUES  ('$id' , '$name', '$email' , '$subject', '$feedback' , '$date' , '$time')")or die ("Error");
+
 
 
